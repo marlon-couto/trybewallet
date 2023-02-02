a3c8394cd6e664a48320a24e324086c65603dae7
Marlon Couto <marloncouto.dev@gmail.com>
2023-02-02 17:25:12 -0300

    O componente Table é alimentado pelo estado da aplicação

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function Table() {
+  const expenses = useSelector((state) => state.wallet.expenses);
+
   return (
     <table>
       <thead>
@@ -16,6 +19,34 @@ export default function Table() {
           <th>Editar/Excluir</th>
         </tr>
       </thead>
+
+      <tbody>
+        {expenses.map(
+          ({
+            value,
+            description,
+            currency,
+            method,
+            tag,
+            id,
+            exchangeRates,
+          }) => (
+            <tr key={ id }>
+              <td>{description}</td>
+              <td>{tag}</td>
+              <td>{method}</td>
+              <td>{parseFloat(value).toFixed(2)}</td>
+              <td>{exchangeRates[currency].name}</td>
+              <td>{parseFloat(exchangeRates[currency].ask).toFixed(2)}</td>
+              <td>
+                {parseFloat(value * exchangeRates[currency].ask).toFixed(2)}
+                {' '}
+              </td>
+              <td>Real</td>
+            </tr>
+          ),
+        )}
+      </tbody>
     </table>
   );
 }
