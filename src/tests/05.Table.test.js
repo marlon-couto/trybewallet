4e11c856385f49c25a59e0df2c6bad10c5127485
Marlon Couto <marloncouto.dev@gmail.com>
2023-02-04 10:24:28 -0300

    Escreve testes para o componente Table

---
+++ src/tests/05.Table.test.js
@@ -0,0 +1,39 @@
+import { screen } from '@testing-library/react';
+import { renderWithRouterAndRedux } from './helpers/renderWith';
+import { initialEntries } from './helpers/mocks';
+
+import App from '../App';
+
+describe('5 - Realiza os testes do componente Table', () => {
+  it('a tabela é renderizada com sucesso', () => {
+    renderWithRouterAndRedux(<App />, { initialEntries });
+
+    expect(
+      screen.queryByRole('columnheader', { name: /descrição/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByRole('columnheader', { name: /tag/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByRole('columnheader', { name: /método de pagamento/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByRole('columnheader', { name: /^valor$/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByRole('columnheader', { name: /^moeda$/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByRole('columnheader', { name: /câmbio utilizado/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByRole('columnheader', { name: /valor convertido/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByRole('columnheader', { name: /moeda de conversão/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByRole('columnheader', { name: /editar\/excluir/i }),
+    ).toBeInTheDocument();
+  });
+});
